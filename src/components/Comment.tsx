5b9837c905c56ace162f39d4509474771ed20bf8
raissa <raissa.campos26@gmail.com>
2022-10-31 15:31:51 -0300

    feat: add ts in project 01 ignite

---
+++ src/components/Comment.tsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import { ThumbsUp, Trash } from "phosphor-react";
+import { Avatar } from "./Avatar";
+import styles from "./Comment.module.css";
+
+interface CommentProps {
+  content: string,
+  onDeleteComment: (comment: string) => void;
+}
+
+export function Comment({ content, onDeleteComment }: CommentProps) {
+  const [likeCount, setLikeCount] = useState(0);
+
+  function handleDeleteComment() {
+    onDeleteComment(content);
+  }
+
+  return (
+    <div className={styles.comment}>
+      <Avatar hasBorder={false} src="http://github.com/proudynyu.png" alt="" />
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Igor Becker</strong>
+              <time title="27 de outubro de 2022" dateTime="2022-10-27">
+                Cerca de 1h atrás
+              </time>
+            </div>
+            <button onClick={handleDeleteComment} title="Deletar comentário">
+              <Trash size={20} />
+            </button>
+          </header>
+          <p>{content}</p>
+        </div>
+        <footer>
+          <button
+            onClick={() => {
+              setLikeCount(likeCount + 1);
+            }}
+          >
+            <ThumbsUp />
+            Aplaudir <span>{likeCount}</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
